c4d0ba2d063d94a7419e4d5648deeaa6b7357e47
Lancelot Salavert <lancelot.salavert@gmail.com>
2016-02-12 10:50:25 +0100

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create, :destroy]
   end
 
+mount Attachinary::Engine => "/attachinary"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
